7cdee8dbc0ef29566a6e431bdf6977f22f8288b5
Aditya Kumar <aadi678a@gmail.com>
2025-07-28 09:41:07 +0530

    updted the video

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover"
         >
-          <source src="/home.mp4" type="video/mp4" />
+          <source src="/main2.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
